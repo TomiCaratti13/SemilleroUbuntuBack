dac97b3cc3970729d37e43d0d6908fc9d49444eb
Herrera <urielherrera412@gmail.com>
2024-02-29 17:51:05 -0300

    entidad usuario, servicio,repositorio,Enum con roles, primera vista de prueba en html

---
+++ src/main/java/com/semillero/ubuntu/controllers/userController.java
@@ -0,0 +1,19 @@
+package com.semillero.ubuntu.controllers;
+/*
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
+
+@RestController
+@RequestMapping("/")
+@RequiredArgsConstructor
+public class userController {
+    @PostMapping(value ="login")
+    public RedirectView userLogin(@ModelAttribute LoginRequest request,){
+        System.out.println(request);
+        return new RedirectView("/") ;
+    }
+}*/
+
